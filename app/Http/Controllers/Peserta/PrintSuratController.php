3980a626cf3148c29ed8b10a58651a340f05015f
Asep Saepuloh <warkirasep.puloh@gmail.com>
2020-10-30 16:00:52 +0700

    Add Nilai Page

--- app/Http/Controllers/Peserta/PrintSuratController.php
+++ app/Http/Controllers/Peserta/PrintSuratController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Peserta;
 use App\Http\Controllers\Controller;
 use App\PKL;
 use Carbon\Carbon;
+use Dompdf\Dompdf;
 use PDF;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use App\Penilaian;
 
 
 class PrintSuratController extends Controller
@@ -14,11 +16,22 @@ class PrintSuratController extends Controller
     public function __invoke(PKL $pkl)
     {
         abort_unless($pkl->tanggal_selesai != null, 403);
-
         $qrcode = QrCode::size(100)
             ->format('svg')
             ->generate('Eko Sulistyono', storage_path('app/public/qrcodes/'.'qrcode'.'.svg'));
         $pkl->load('peserta.sekolah');
+        $user = auth()->user();
+
+        $nilai = Penilaian::with('kategori')->whereHas('pkl', function ($query) use ($user) {
+            return $query->where('peserta_id', $user->authenticable_id);
+        })->get();
+        //dd($nilai);
+
+        $sum = $nilai->sum('nilai');
+        $count = $nilai->count();
+        $total = $count > 0 ? round($sum / $count, 2) : 0;
+        $data['nilai'] = $nilai;
+        $data['total'] = $total;
 
         $data['nama'] = $pkl->peserta->nama;
         $data['nis'] = $pkl->peserta->nis;
@@ -28,7 +41,7 @@ class PrintSuratController extends Controller
         $data['tanggal'] = $data['tglSelesai'];
         $data['qrcode'] = $qrcode;
         $pdf = PDF::loadView('peserta.print-surat', ['data' => $data])->setPaper('A4');
-        //return $pdf->stream();
-        return $pdf->download('surat-keterangan-selesai-pkl.pdf');
+        return $pdf->stream();
+        //return $pdf->download('surat-keterangan-selesai-pkl.pdf');
     }
 }
--- resources/views/peserta/print-surat.blade.php
+++ resources/views/peserta/print-surat.blade.php
@@ -52,6 +52,11 @@
         .justify-content-end {
             justify-content: flex-end !important
         }
+
+        .ttd {
+            position: fixed;
+            bottom: 250px;
+        }
     </style>
 </head>
 <body>
@@ -119,6 +124,74 @@
             <img src="{{ storage_path('app/public/qrcodes/'.'qrcode'.'.svg') }}" alt="">
             <br>
             <p>Eko Sulistyono</p>
+        </div>
+    </div>
+</div>
+<div style="page-break-before: always;">
+    <div class="container" style="padding-left: 10%;padding-right: 10%">
+        <br>
+        <div id="watermark"><img src="{{ public_path('/assets/img/km.png') }}" alt="Watermark" style="width: 400px"/></div>
+        <div class="row">
+            <div class="col"><br><br>
+                <div class="text-center">
+                    <h3><strong>FORM NILAI PRAKTIK KERJA LAPANGAN <br> KAMPUNG MARKETER</strong></h3>
+                </div>
+                <br>
+                <br>
+                <table class="w-100">
+                    <tr>
+                        <td width="12.5%">Nama</td>
+                        <td width="1.5%">:</td>
+                        <td>{{ $data['nama'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>NIS</td>
+                        <td>:</td>
+                        <td>{{ $data['nis'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Sekolah</td>
+                        <td>:</td>
+                        <td>{{ $data['sekolah'] }}</td>
+                    </tr>
+                </table>
+                <br>
+                <br>
+                <p>Dengan penilaian sebagai berikut :</p>
+                <table class="w-100" style="border-collapse: collapse;">
+                    <thead>
+                        <tr>
+                            <th style="border: 1px solid #000000;">Kategori</th>
+                            <th style="border: 1px solid #000000;">Nilai</th>
+                            <th style="border: 1px solid #000000;">Keterangan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data['nilai'] as $item)
+                        <tr>
+                            <td style="border: 1px solid #000000; text-align: center;">{{ $item->kategori->nama }}</td>
+                            <td style="border: 1px solid #000000; text-align: center;">{{ $item->nilai }}</td>
+                            <td style="border: 1px solid #000000; text-align: center;">{{ $item->keterangan }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td style="border: 1px solid #000000; text-align: center;">Nilai Akhir</td>
+                            <td style="border: 1px solid #000000; text-align: center;" colspan="2">{{ $data['total'] }}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+        <div style="text-align: right" class="ttd">
+            <div>
+                <p>Purbalingga, {{ $data['tanggal'] }}</p>
+                <br>
+                <img src="{{ storage_path('app/public/qrcodes/'.'qrcode'.'.svg') }}" alt="">
+                <br>
+                <p>Eko Sulistyono</p>
+            </div>
         </div>
     </div>
 </div>
